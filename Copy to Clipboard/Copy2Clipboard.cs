7f46961d015007750a9336fc772f385da47b331c
Duc Filan <ducfilan@gmail.com>
2016-01-28 01:17:24 +0700

    First commit

---
+++ Copy to Clipboard/Copy2Clipboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using Copy_to_Clipboard.Properties;
+
+namespace Copy_to_Clipboard
+{
+    public partial class Copy2Clipboard : Form
+    {
+        public Copy2Clipboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Copy2Clipboard_Load(object sender, System.EventArgs e)
+        {
+            Clipboard.SetText(Settings.Default.Text);
+            Close();
+        }
+    }
+}
---
+++ Copy to Clipboard/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Copy 2 Clipboard")]
+[assembly: AssemblyDescription("Copy Text 2 Clipboard")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Duc Filan")]
+[assembly: AssemblyProduct("Copy to Clipboard")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Duc Filan")]
+[assembly: AssemblyTrademark("Duc Filan")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("330bca94-9ac1-41a0-8703-01c7dcaad390")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
